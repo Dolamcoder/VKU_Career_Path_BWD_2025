944360dd52137336470ddf81cc2a59d993489247
DO LAM <thinhphu479@gmail.com>
2025-05-04 19:27:43 +0700

    update

--- VKU_Career_Path/src/public/JavaScript/effect.js
+++ VKU_Career_Path/src/public/JavaScript/effect.js
@@ -2,43 +2,6 @@
     AOS.init({
     duration: 1000, 
     });
-// headerScroll
-    window.onscroll=function(){
-    if(document.body.scrollTop>200 || document.documentElement.scrollTop>200){
-        document.getElementById("scroll").style.transform="translateY(0%)";
-    }
-    else{
-        document.getElementById("scroll").style.transform="translateY(-100%)";
-    }
-    }
-  // Mobile menu toggle
-  const menuMobileButton = document.getElementById('menuMobile');
-  const menuMobile = document.getElementById('menu_mobile');
-  const closeMenuMobile = document.getElementById('closeMenuMobile');
-  const menuMobileButtonScroll = document.getElementById('menuMobileScroll');
-  const menuMobileScroll = document.getElementById('menu_mobile_scroll');
-  const closeMenuMobileScroll = document.getElementById('closeMenuMobileScroll');
-  
-  menuMobileButton.addEventListener('click', () => {
-    menuMobileButton.classList.add('animation_xoay');
-    menuMobile.style.transform = 'translateX(0%)';
-  });
-  
-  closeMenuMobile.addEventListener('click', () => {
-    closeMenuMobile.classList.add('animation_xoay');
-    menuMobile.style.transform = 'translateX(100%)';
-  });
-  
-  menuMobileButtonScroll.addEventListener('click', () => {
-    menuMobileButtonScroll.classList.add('animation_xoay');
-    menuMobileScroll.style.transform = 'translateX(0%)';
-  });
-  
-  closeMenuMobileScroll.addEventListener('click', () => {
-    closeMenuMobileScroll.classList.add('animation_xoay');
-    menuMobileScroll.style.transform = 'translateX(100%)';
-  });
-  
   // Counter animation
   const counters = document.querySelectorAll('.counter');
   const speed = 200;
