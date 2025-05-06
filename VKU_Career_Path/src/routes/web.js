c8219bb022eba5bde7691b405612c677794ee1cd
qucank2006 <anhtlq.24itb@vku.udn.vn>
2025-05-06 15:07:08 +0700

    test

--- VKU_Career_Path/src/routes/web.js
+++ VKU_Career_Path/src/routes/web.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const {getHome, getAdvise}=require('../controller/indexController');
+const {getHome, getAdvise, getLogin}=require('../controller/indexController');
 //router.get(route, handler);
 router.get('/', getHome);
 router.get('/advise', getAdvise);
+router.get('/login', getLogin)
 module.exports = router;
